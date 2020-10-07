ad1262835c086cc92694d413ff19301b429af0f7
fcsouza <fcsouza18@hotmail.com>
2020-10-07 19:37:01 -0300

    include event trigger

--- node/clients/analytics.ts
+++ node/clients/analytics.ts
@@ -1,5 +1,6 @@
 import { AppClient, InstanceOptions, IOContext } from '@vtex/api'
 
+
 export default class Analytics extends AppClient {
   constructor(context: IOContext, options?: InstanceOptions) {
       super('vtex.mocked-analytics@0.x', context, options)
